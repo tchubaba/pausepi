5ecacd6902faba1d2988e0b22b6d61de77d1e0a2
Daniel da Silva <cdaniel.dasilva@gmail.com>
2024-07-31 01:10:09 -0500

    Fix type issue in pausepi config file

--- config/pausepi.php
+++ config/pausepi.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'min_timeout_seconds' => env('PAUSEPI_MIN_TIMEOUT_SECONDS', 30),
-    'max_timeout_seconds' => env('PAUSEPI_MAX_TIMEOUT_SECONDS', 300),
+    'min_timeout_seconds' => (int) env('PAUSEPI_MIN_TIMEOUT_SECONDS', 30),
+    'max_timeout_seconds' => (int) env('PAUSEPI_MAX_TIMEOUT_SECONDS', 300),
 ];
